d335b27a02676e9f04a6e519f31382b88536bc8d
Lyutenkov Nikita <Lyutenkoff.N@gmail.com>
2025-03-05 17:25:34 +0300

    added pmd to project

--- build.gradle.kts
+++ build.gradle.kts
@@ -1,6 +1,12 @@
 // Copyright 2000-2025 JetBrains s.r.o. and contributors. Use of this source code is governed by the Apache 2.0 license.
 plugins {
   java
+  pmd
+}
+
+pmd {
+  toolVersion = "6.55.0"
+  ruleSetFiles = files("pmd.xml")
 }
 
 tasks.compileJava {
